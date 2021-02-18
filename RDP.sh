8192a1171c4442c7d743f51876b1aa9852c26594
ollydbg-1 <44168374+ollydbg-1@users.noreply.github.com>
2021-02-18 13:35:10 +0100

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -21,7 +21,7 @@ sudo apt install nautilus nano -y
 sudo adduser BRAF chrome-remote-desktop
 } &> /dev/null &&
 
-
+setxkbmap fr 
 sudo apt-get install transmission
 sudo apt-get install unrar
 sudo wget  https://az764295.vo.msecnd.net/stable/622cb03f7e070a9670c94bae1a45d78d7181fbd4/code_1.53.2-1613044664_amd64.deb
